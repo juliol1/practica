e0e6e9a5b4ad0f4d3de0ff6bcb9a64c414df738b
juliol1 <juliovillalvazo26@gmail.com>
2021-10-16 01:21:04 -0500

    realice cambios de los menus de navegación

--- js/toggle.js
+++ js/toggle.js
@@ -1,6 +1,16 @@
 const toggle = document.getElementById('toggle');
 const nav = document.getElementById('mobile-menu');
+const main = document.getElementById('main');
+const header = document.getElementById('navbar');
 
 toggle.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
+
+main.addEventListener('click', () => {
+    nav.classList.remove('active');
+})
+
+header.addEventListener('click', () => {
+    nav.classList.remove('active');
+})
